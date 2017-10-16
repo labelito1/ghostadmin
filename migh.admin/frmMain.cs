724cec1be95c21e8cded28b3f007a32a1118a748
505darksoft <eduardo.hley@gmail.com>
2017-10-15 22:58:09 -0500

    ff

--- migh.admin/frmMain.cs
+++ migh.admin/frmMain.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using migh.api;
 using JsonTools;
 using simple.ftp;
-
+using System.IO;
 namespace migh.admin
 {
     public partial class frmMain : Form
@@ -69,11 +69,98 @@ namespace migh.admin
             frm.ShowDialog();
         }
         int SessionVersion = 0;
+
+
+        class newArtist
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string image { get; set; }
+            public string image_small { get; set; }
+        }
+        class 
+            newAlbum
+        {
+            public int id { get; set; }
+            public string title { get; set; }
+            public string artist { get; set; }
+            public string cover { get; set; }
+            public string cover_small { get; set; }
+            public int year { get; set; }
+            public int artist_id { get; set; }
+        }
+        class newTrack
+        {
+            public int id { get; set; }
+            public string title { get; set; }
+            public string artist { get; set; }
+            public string JoinedPerformers { get; set; }
+            public string album { get; set; }
+            public string duration { get; set; }
+            public int tracknumber { get; set; }
+            public int album_id { get; set; }
+            public int artist_id { get; set; }
+            public string hq_src { get; set; }
+            public string mq_src { get; set; }
+            public string lq_src { get; set; }
+        }
         private void btnSave_Click(object sender, EventArgs e)
         {
             admin.Library.Version = SessionVersion;
             //admin.Library.configuration = Configuration.Data;
             Library.write_local(admin.Library, Configuration.LibraryFileName);
+
+            List<newArtist> artist_list = new List<newArtist>();
+            foreach (Artist art in admin.Library.artist_list)
+            {
+                newArtist newart = new newArtist();
+                newart.id = art.id;
+                newart.name = art.name;
+                newart.image = string.Format("https://github.com/ghostapphd/artistimages/blob/master/{0}/image.jpg?raw=true", Tools.ConvertToGitHubFolder(art.name));
+                newart.image_small = string.Format("https://github.com/ghostapphd/artistimages/blob/master/{0}/imagesmall.jpg?raw=true", Tools.ConvertToGitHubFolder(art.name));
+                artist_list.Add(newart);
+            }
+            File.WriteAllText(Environment.CurrentDirectory + "/ghost-artists.json", artist_list.OrderBy(a => a.name).ToJsonString());
+
+            List<newAlbum> album_list = new List<newAlbum>();
+            foreach (Album alb in admin.Library.album_list)
+            {
+                newAlbum newalb = new newAlbum();
+                Artist art = admin.Library.artist_list.FirstOrDefault(ar => ar.id == alb.artist_id);
+                newalb.id = alb.id;
+                newalb.title = alb.name;
+                newalb.artist = art.name;
+                newalb.cover = string.Format(admin.Library.configuration.AlbumCoverImageFileURLFormat, Tools.ConvertToGitHubFolder(art.name), Tools.ConvertToGitHubFolder(alb.name));
+                newalb.cover_small = string.Format(admin.Library.configuration.AlbumCoverImageFileURLFormat.Replace("Cover.jpg", "CoverSmall.jpg"), Tools.ConvertToGitHubFolder(art.name), Tools.ConvertToGitHubFolder(alb.name));
+                newalb.year = Convert.ToInt32(alb.year);
+                newalb.artist_id = alb.artist_id;
+                album_list.Add(newalb);
+            }
+
+            File.WriteAllText(Environment.CurrentDirectory + "/ghost-albums.json", album_list.OrderByDescending(a => a.year).ToJsonString());
+
+            List<newTrack> track_list = new List<newTrack>();
+            foreach (Song sng in admin.Library.song_list)
+            {
+                newTrack newt = new newTrack();
+                Artist art = admin.Library.artist_list.FirstOrDefault(ar => ar.id == sng.artist_id);
+                Album alb = admin.Library.album_list.FirstOrDefault(al => al.id == sng.album_id);
+                newt.id = sng.id;
+                newt.title = sng.name;
+                newt.artist = art.name;
+                newt.JoinedPerformers = sng.JoinedPerformers;
+                newt.album = alb.name;
+                newt.duration = sng.duration.ToString("mm\\:ss");
+                newt.tracknumber = Convert.ToInt32(sng.Track);
+                newt.album_id = sng.album_id;
+                newt.artist_id = sng.artist_id;
+                newt.hq_src = string.Format("https://raw.githubusercontent.com/ghostapphd/{0}/master/{1}/{2}.m4a", Tools.ConvertToGitHubFolder(art.name), Tools.ConvertToGitHubFolder(alb.name), Song.getFileFormat(sng));
+                newt.mq_src = string.Format("https://raw.githubusercontent.com/ghostapphdmq/{0}/master/{1}/{2}.mp3", Tools.ConvertToGitHubFolder(art.name), Tools.ConvertToGitHubFolder(alb.name), Song.getFileFormat(sng));
+                newt.lq_src = string.Format("https://raw.githubusercontent.com/ghostapphdlq/{0}/master/{1}/{2}.opus", Tools.ConvertToGitHubFolder(art.name), Tools.ConvertToGitHubFolder(alb.name), Song.getFileFormat(sng));
+                track_list.Add(newt);
+            }
+            File.WriteAllText(Environment.CurrentDirectory + "/ghost-tracks.json", track_list.OrderBy(t => t.tracknumber).ToJsonString());
+
             MessageBox.Show("Guardado");
             if (chkSubir.Checked)
             {
